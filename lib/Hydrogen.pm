0f4876eb18c9780e3feb28dfd54b562d478f58f7
Toby Inkster <mail@tobyinkster.co.uk>
2022-08-27 17:51:58 +0100

    Oops, that no work

--- lib/Hydrogen.pm
+++ lib/Hydrogen.pm
@@ -69,18 +69,6 @@ Normal version of the function:
     say $hash{Alice};                ## ==> 123
     say hhr_get( \%hash, 'Bob' );    ## ==> 456
 
-Autoboxing, kinda:
-
-    use feature 'say';
-    use Hydrogen::HashRef ();
-    
-    my $hashref = {};
-    $hashref->Hydrogen::HashRef::set( Alice => 123 );
-    $hashref->Hydrogen::HashRef::set( Bob   => 456 );
-    
-    say $hashref->{Alice};                            ## ==> 123
-    say $hashref->Hydrogen::HashRef::get( 'Bob' );    ## ==> 456
-
 Version of the function which uses prototypes:
 
     use feature 'say';
